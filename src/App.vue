e9f10fd4d79b551859c0dd2a923220117684399f
Therese Linden <theresemlinden@gmail.com>
2023-07-07 18:55:21 +0200

    nav link routes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <script setup></script>
 
 <template>
-  <header></header>
+  <header>
+    <nav>
+      <ul>
+        <router-link :to="{ name: 'Home' }">Home</router-link>
+        <router-link :to="{ name: 'Contact' }">Contact</router-link>
+      </ul>
+    </nav>
+  </header>
   <main>
     <router-view />
   </main>
